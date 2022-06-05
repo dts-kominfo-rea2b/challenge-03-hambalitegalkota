fa8f829735a17c36821deba829006cbe1906aa4c
Ali Hambali Hidayat <105773421+hambalitegalkota@users.noreply.github.com>
2022-06-05 22:05:08 +0700

    maaf masih belum paham, perlu belajar lagi

--- index.js
+++ index.js
@@ -21,7 +21,21 @@ const dataBelanjaan = [
 ];
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const listBelanjaan = null;
+const listBelanjaan = () => {
+  const belanjaan = [
+    { "id": "D-10001", "nama": "Minyak Goreng Delima", "harga: "25000", "kuantitas": "2" },
+    { "id": "D-10020, "nama": "Beras Mamos", "harga: "48000", "kuantitas": "1" },
+    { "id": "F-00010", "nama": "Larutan Cap Kaki Empat", "harga: "7500", "kuantitas": "8" }
+  ];
+  
+  return belanjaan:
+};
+
+const objectBelanjaan = listBelanjaan();
+console.log(${objeckBelanjaan}[0].nama, "x" .harga)
+console.log(${objeckBelanjaan}[1].nama, "x" .harga)
+console.log(${objeckBelanjaan}[2].nama, "x" .harga)
+
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
 const totalBelanjaan = null;
